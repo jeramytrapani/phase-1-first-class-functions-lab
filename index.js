1710ee67a67e4aa18b0770f5ea8d5767ee6b579e
jeramytrapani <jeramytrapani@gmail.com>
2021-08-01 04:01:33 -0500

    Second Save

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const firstTwoDrivers = function (driverOne, driverTwo, driverThree) {
-    return driverOne
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0,2)
 }
 
-// function returnFirstTwoDrivers(driverOne, driverTwo, driverThree) {
-//     const firstTwoDrivers = []
-// }
+const returnLastTwoDrivers = function (drivers){
+   // return drivers.splice(drivers.length - 2)
+    return drivers.slice(drivers.length - 2)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(fareMultiplier){
+    return fareMultiplier; 
+}
